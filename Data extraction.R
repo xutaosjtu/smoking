bc5e3a5f9c409e0d175be4d3e8207f4d107cead4
Tao Xu <xutaosjtu@gmail.com>
2013-10-07 14:24:03 +0200

    + Extracting a subset of candidates for the casual inference analysis

---
+++ Data extraction.R
@@ -0,0 +1,58 @@
+files = dir("methylation/F3", pattern="*txt.gz", full.names = T)
+files = dir("methylation/F4", pattern="*txt.gz", full.names = T)
+files = files[1:22]
+#################################################################
+## run the code in mehtylation analysis to obtain cpg.confirmed
+#################################################################
+methy.data = foreach(i = 1:length(files), .combine = rbind ) %dopar% {
+	f <- gzfile(files[i], "r")
+	line=readLines(f,n=1)
+	samples = unlist(strsplit(line, split = " "))[-1]
+	samples = substr(samples, 2, 10)
+	line=readLines(f,n=1)
+	data = NULL
+	while( length(line) != 0 ) {
+		 tmp = unlist(strsplit(line, split = " "))
+		 if(tmp[1] %in% cpg.confirmed){
+			data = rbind(data, tmp)
+		 }
+		 line=readLines(f,n=1)
+	}
+	#colnames(data) = samples
+	close(f)
+	return(data)
+}
+
+rst = NULL
+for(i in files){
+	f<-gzfile(i, "r")
+	line = readLines(f, n=1)
+	samples = unlist(strsplit(line, split = " "))[-1]
+	samples = substr(samples, 2, 10)
+	close(f)
+	rst = rbind(rst, samples)
+}
+
+
+
+#################################################################
+## Candidates of differentially expressed genes: CLDND1, LRRN3
+#################################################################
+
+
+
+#################################################################
+## Candidate metabolites according to publication in BMC medicine 
+#################################################################
+probes = c("ILMN_1773650","ILMN_2048591" ##LRRN3
+		,"ILMN_1710326", "ILMN_2352563"  ##CLDND1
+		)
+
+F3.expression = read.csv("expression/KORA_F3_379_samples_normalized_expression_data_zzupdated.csv", row.names = 1)
+F4.expression = load("Expression_s4_adjusted_technical_variables")
+
+F3.expression = F3.expression[probes,]
+F4.expression = F4.expression[probes,]
+
+
+
